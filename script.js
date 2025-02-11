d44ac72c2cb569dd5c0d714b59238d4be55e3ea6
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-11 11:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //complete this code
-class Persion{
+class Person{
     constructor(name,age){
         this._name = name;
         this._age = age;
@@ -10,35 +10,35 @@ class Persion{
     }
     
     set age(p_age){
-        this._age = p_age
+        this._age = p_age;
     }
 }
-class Student extends Persion{
+class Student extends Person{
     study(){
         console.log(`${this._name} is studying`);
     }
 }
 
-class Teacher extends Persion{
+class Teacher extends Person{
     teach(){
         console.log(`${this._name} is teaching`);
     }
 }
-// testing
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person._name);  // Output: John
+// // testing
+// // Creating a Person instance
+// const person = new Person("John", 25);
+// console.log(person._name);  // Output: John
 
-person._age = 30;  // Using the setter to change the age
-console.log(person._age);  // Output: 30
+// person._age = 30;  // Using the setter to change the age
+// console.log(person._age);  // Output: 30
 
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
+// // Creating a Student instance
+// const student = new Student("Alice", 22);
+// student.study();  // Output: Alice is studying
 
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Output: Bob is teaching
+// // Creating a Teacher instance
+// const teacher = new Teacher("Bob", 40);
+// teacher.teach();  // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
